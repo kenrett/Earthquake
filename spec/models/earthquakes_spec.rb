a5dbf97683480f62266a30c76a81c63f659daf8b
kenrett <kenrettberg@gmail.com>
2014-01-20 18:25:04 -0800

    added test for default scope

--- spec/models/earthquakes_spec.rb
+++ spec/models/earthquakes_spec.rb
@@ -1,9 +1,20 @@
 require 'spec_helper'
 
 describe Earthquake do
- describe 'default scope' do
+  describe 'default scope' do
     it 'should only show last 7 days data' do
       Earthquake.all.to_sql.should match(/quake_date['"]? >= '?#{7.days.ago.to_date}/)
     end
-  end
+
+  describe '.on_day(date)' do
+    it 'should select only those where quake_date lies within date given' do
+      today = Date.today
+      Earthquake.unscoped.on_date(today).to_sql.should match(/date\(.*quake_date.*\) = '#{today.to_s(:db)}'/)
+    end
+  end 
+
+  
+end
+
+
 end
