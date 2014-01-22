8d7768cb40d19aa17707e8f1e68a2a58cf9924ac
kenrett <kenrettberg@gmail.com>
2014-01-21 17:08:03 -0800

    added DatabaseCleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,10 +28,10 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = 'random'
 
-  # config.before(:suite) do
-  #   DatabaseCleaner.strategy = :transaction
-  #   DatabaseCleaner.clean_with(:truncation)
-  # end
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
 
   config.before(:each) do
     DatabaseCleaner.start
